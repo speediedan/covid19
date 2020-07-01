9277e10a160d9df7f1d68a7d9565bd28c212cb20
Daniel Dale <dan@distributedinsight.com>
2020-06-30 17:34:38 -0700

    autogenerated dashboard update for 20200630

--- dashboard_gen.sh
+++ dashboard_gen.sh
@@ -69,9 +69,11 @@ if [[ ${dash_gen} -eq 0 ]]; then
   fi
   echo "Cleaning up old logs"
   find "${LOG_DIR}/dashboard_generation_logs" -name 'covid19_dashboard.out_20*' -mtime +3 -print | xargs rm -rf
+  /opt/anaconda/envs/${target_env}/bin/python ${COVID_BASE}/notification.py -f "${dash_logfile}"
   rm $lock_file
 else
   echo "Dashboard generation failed. Output code was: ${dash_gen}" >> $dash_logfile
+  /opt/anaconda/envs/${target_env}/bin/python ${COVID_BASE}/notification.py -f "${dash_logfile}" -s "Dashboard Generation Failure!"
   rm $lock_file
   exit 1
 fi
--- notification.py
+++ notification.py
@@ -1,49 +1,50 @@
 import smtplib
-from typing import List
-from pathlib import Path
 import os
 from email.message import EmailMessage
+import config
+import traceback
 import sys
+from argparse import ArgumentParser
 
 
-with open(textfile) as fp:
-    # Create a text/plain message
-
-    msg.set_content(fp.read())
-
-# me == the sender's email address
-# you == the recipient's email address
-msg['Subject'] = f'The contents of {textfile}'
-msg['From'] = me
-msg['To'] = you
-
-
-sent_from = gmail_user
-to = ['me@gmail.com', 'bill@gmail.com']
-subject = 'OMG Super Important Message'
-body = 'Hey, what's up?\n\n- You'
-
-email_text = """\
-From: %s
-To: %s
-Subject: %s
-
-%s
-""" % (sent_from, ", ".join(to), subject, body)
-def send_gmail_notification(subject, from_addr, to_addr, email_content):
+def send_gmail_notification(email_content: str, mail_subject: str, to_addr: str) -> None:
     # collect mail params
     mail_dict = {}
     for mail_key in ['MAIL_USER', 'MAIL_APP_PASS']:
-        mail_dict[mail_key] = os.environ[mail_key] if mail_key in os.environ.keys() else None
+        if mail_key in os.environ.keys():
+            mail_dict[mail_key] = os.environ[mail_key]
+        else:
+            raise ValueError(f"Missing environmental parameter {mail_key} required for authentication. Exiting")
     try:
-        msg = EmailMessage()
-        msg['Subject']
+        with open(email_content) as fp:
+            msg = EmailMessage()
+            msg.set_content(fp.read())
+        msg['Subject'] = mail_subject
+        msg['From'] = f"{mail_dict['MAIL_USER']}@gmail.com"
+        msg['To'] = to_addr if to_addr else f"{mail_dict['MAIL_USER']}@gmail.com"
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.ehlo()
-        server.login(gmail_user, gmail_password)
-        server.sendmail(sent_from, to, email_text)
+        server.login(mail_dict['MAIL_USER'], mail_dict['MAIL_APP_PASS'])
+        server.sendmail(msg['From'], msg['To'], msg.as_string())
         server.close()
+        print('notification email sent')
+    except Exception as e:  # a lot could go wrong here. for now, shamefully using a broad except/logging traceback
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        print(f"error sending notification email: "
+              f"{repr(traceback.format_exception(exc_type, exc_value, exc_traceback))}")
+        raise e
 
-        print 'Email sent!'
-    except:
-        print 'Something went wrong...'
+
+def main():
+    parser = ArgumentParser(description="""\
+Sends a gmail email using a from email and password specified via env variables MAIL_USER and MAIL_APP_PASS respectively
+""")
+    parser.add_argument('-f', '--file', required=True, dest='email_content', help="""file to send in the email""")
+    parser.add_argument('-s', '--subject', dest='mail_subject', default=config.mail_subject, help='The email subject')
+    parser.add_argument('-t', '--to', required=False, dest='to_addr', default=None, help='recipient of email')
+    args = parser.parse_args()
+    send_gmail_notification(args.email_content, args.mail_subject, args.to_addr)
+
+
+if __name__ == '__main__':
+    main()
